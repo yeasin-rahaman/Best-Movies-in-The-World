1a17dba443436756b577976edd4ab7e692f582df
Dev70 <Shanto9585>
2021-09-26 17:10:35 +0600

    update

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,7 +4,7 @@ import '../Cart/Cart.css'
 const Cart = (props) => {
     const { cart } = props || {}
 
-
+    // calculate total 
     const totalReducer = (prevValue, currentValue) => prevValue + currentValue.earn
 
     const total = cart.reduce(totalReducer, 0)
@@ -12,7 +12,7 @@ const Cart = (props) => {
 
     return (
         <div className="">
-
+            {/* cart  */}
             <div className="card cart-bg">
                 <h3>  <i className="fas fa-film mt-5"></i> Number of Movies:  {cart.length} </h3>
                 <hr className="line-for-cart" />
@@ -22,15 +22,14 @@ const Cart = (props) => {
                     <div className='cart-movie'>
                         <ul>
                             {
+                                // cart price and name 
                                 cart.map(movie =>
-
                                     <li className="d-flex p-3 line-for-cart" key={movie.key}> <img src={movie.img} alt="" height="100" width="100" className="pe-3" />
                                         <div> <b>{movie.name}</b>
-                                            <p>Earns:  {movie.earn}$</p>
-
+                                            <br />
+                                            <br />
+                                            <small>Earns:  {movie.earn}$</small>
                                         </div>
-
-
                                     </li>)
 
                             }
